1e272fd1e5543b2ad2189e05213192b0c05af579
conatuseus <conatuseus@gmail.com>
2019-04-03 13:37:59 +0900

    사용자 정의 태그 만들기
    class 사용자 정의태그 이름 extends Component {
    	render(){
    		return(
    		);
    	}
    }
    이런식으로 만들 수 있다.
    리액트는 이런 사용자 정의 태그를 만들 수 있는 큰 장점이 있다.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+class Subject extends Component{
+  render(){
+    return (
+      <header>
+        <h1>WEB</h1>
+        World Wide Web
+      </header>
+    );
+  }
+}
+
+class TOC extends Component{
+  render(){
+    return (
+      <nav>
+        <ol>
+          <li><a href="1.HTML"> HTML</a></li>
+          <li><a href="2.CSS"> CSS </a></li>
+          <li><a href="3.JAVASCRIPT"> JAVASCRIPT</a></li>
+        </ol>
+      </nav>
+    );
+  }
+}
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App"> 
+        
+        <Subject></Subject>
+        <TOC></TOC>
       </div>
     );
   }
