afcb898292f601c29ae9a50993ec1763bb758658
conatuseus <conatuseus@gmail.com>
2019-04-03 13:55:12 +0900

    property 사용
    props를 사용해서 재사용의 활용성을 늘린다

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ class Subject extends Component{
   render(){
     return (
       <header>
-        <h1>WEB</h1>
-        World Wide Web
+        <h1>{this.props.title}</h1>
+        {this.props.sub}
       </header>
     );
   }
@@ -25,14 +25,28 @@ class TOC extends Component{
   }
 }
 
+class Content extends Component {
+  render(){
+      return (
+          <article>
+            <h2>{this.props.title}</h2>
+            {this.props.desc}
+          </article>
+      );
+  }
+
+}
+
 
 class App extends Component {
   render() {
     return (
       <div className="App"> 
         
-        <Subject></Subject>
+        <Subject title="React" sub="Hello, React."></Subject>
         <TOC></TOC>
+        <Content title="HTML" desc="HTML is for info"> </Content>
+        <Content title="CSS" desc="CSS is for look"> </Content>
       </div>
     );
   }
